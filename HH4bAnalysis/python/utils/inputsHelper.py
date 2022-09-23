0bebf36545e9f2740ddc4fe426ff8c574ee90b2d
Victor Hugo Ruelas Rivera <victor.hugo.ruelas.rivera@cern.ch>
2022-09-23 17:19:15 +0200

    Add option for disabling trigger filtering

--- HH4bAnalysis/python/utils/inputsHelper.py
+++ HH4bAnalysis/python/utils/inputsHelper.py
@@ -11,3 +11,26 @@ def get_valid_ami_tag(tags, check_tag="p", min_valid_tag=SampleTypes.mc20):
         if check_tag in tag:
             is_valid_tag = int(tag[1:]) > int(min_valid_tag.value[1:])
     return is_valid_tag
+
+
+def get_dataType(flags):
+    if flags.Input.SimulationFlavour in [
+        "",
+        "FullG4",
+        "FullG4_QS",
+        "FullG4_Longlived",
+    ]:
+        dataType = "mc"
+    if flags.Input.SimulationFlavour in ["ATLFAST3_QS"]:
+        # in R22 there are no calibrations for af3 yet,
+        # using FullSim calibrations for now
+        dataType = "mc"
+    if not flags.Input.isMC:
+        dataType = "data"
+
+    try:
+        dataType
+    except NameError:
+        raise AssertionError("dataType cannot be determined from inputs!")
+
+    return dataType
