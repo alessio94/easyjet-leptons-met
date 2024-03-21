fd22965062474d05e5f84db4fbe90fc5ecc5b0d1
Thomas Strebler <thomas.strebler@cern.ch>
2024-03-21 15:45:54 +0100

    GN2v01 is not available yet in p6026 PHYSLITE

--- EasyjetHub/python/output/ttree/small_R_jets.py
+++ EasyjetHub/python/output/ttree/small_R_jets.py
@@ -111,7 +111,8 @@ def get_small_R_bjet_branches(
                 "GN2v00_pu",
             ]
 
-        gn2v01_valid_ptag = get_valid_ami_tag(split_tags, "p", "p6026")
+        gn2v01_valid_ptag = (
+            get_valid_ami_tag(split_tags, "p", "p6026") and not flags.Input.isPHYSLITE)
         if gn2v01_valid_ptag:
             small_R_bjet_branches.variables += [
                 "GN2v01_pb",
