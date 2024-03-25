e4cd508a0114b74c47ae829044418a239a3e90a8
dabattul <daariimaa.battulga@cern.ch>
2024-03-25 15:14:50 +0100

    adding missing truth variable

--- EasyjetHub/python/output/ttree/truth_jets.py
+++ EasyjetHub/python/output/ttree/truth_jets.py
@@ -6,7 +6,7 @@ def get_common_jet_truth_labels(flags):
     if not flags.Input.isPHYSLITE:
         truth_labels += [
             *[f"nTopTo{p}Children" for p in "BW"],
-            *[f"parent{p}ParentsMask" for p in ["Higgs", "Top"]],
+            *[f"parent{p}ParentsMask" for p in ["Higgs", "Scalar", "Top"]],
         ]
 
     return truth_labels
