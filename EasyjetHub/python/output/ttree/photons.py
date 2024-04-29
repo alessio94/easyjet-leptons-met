ae3de8cfec72c29f1db459a6a987c5c664530b51
Luca Franco <luca.franco@cern.ch>
2024-04-29 14:16:14 +0200

    add extra photon variables to the output (HIGG1D1 only)

--- EasyjetHub/python/output/ttree/photons.py
+++ EasyjetHub/python/output/ttree/photons.py
@@ -18,4 +18,33 @@ def get_photon_branches(flags, tree_flags, input_container, output_prefix):
 
     photon_branches.add_four_mom_branches(do_mass=False)
 
+    if tree_flags.collection_options.photons.shower_shapes:
+        photon_branches.variables += [
+            "Rhad",
+            "Rhad1",
+            "Reta",
+            "Rphi",
+            "weta1",
+            "weta2",
+            "f1",
+            "f3",
+            "fracs1",
+            "wtots1",
+            "DeltaE",
+            "Eratio",
+        ]
+
+    if tree_flags.collection_options.photons.iso_variables:
+        photon_branches.variables += [
+            "ptcone20",
+            "topoetcone20",
+            "topoetcone40",
+        ]
+
+    if flags.Input.isMC and tree_flags.collection_options.photons.truth_labels:
+        photon_branches.variables += [
+            "truthType",
+            "truthOrigin",
+        ]
+
     return photon_branches.get_output_list()
