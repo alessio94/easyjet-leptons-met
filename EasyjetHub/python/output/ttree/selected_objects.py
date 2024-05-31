e7cbf2754813966c48e305eee9c4b9c980dcafa9
Thomas Strebler <thomas.strebler@cern.ch>
2024-05-31 09:34:20 +0200

    Save systematics for all selected object branches

--- EasyjetHub/python/output/ttree/selected_objects.py
+++ EasyjetHub/python/output/ttree/selected_objects.py
@@ -8,10 +8,6 @@ def get_selected_objects_branches_variables(flags, analysis):
     float_variable_names = []
     int_variable_names = []
 
-    # Checking if we need to store systematics for variables other than pT or SF.
-    slim_variables_with_syst = \
-        flags.Analysis.ttree_output[0].slim_variables_with_syst
-
     # All jets
     for var in [*flags.Analysis.small_R_jet.variables_int_allJets,
                 *flags.Analysis.small_R_jet.variables_allJets]:
@@ -24,13 +20,9 @@ def get_selected_objects_branches_variables(flags, analysis):
             if var in flags.Analysis.small_R_jet.variables_int_allJets:
                 int_variable_names += [f"Jet{index+1}_{var}"]
             # Translate the name to an analysis specific convention
-            if slim_variables_with_syst and "pt" not in var and "SF" not in var:
-                branches += [f"EventInfo.Jet{index+1}_{var}_NOSYS \
-                            -> {analysis}_Jet{index+1}_{var}"]
-            else:
-                branches += [f"EventInfo.Jet{index+1}_{var}_%SYS% \
-                            -> {analysis}_Jet{index+1}_{var}"
-                             + flags.Analysis.systematics_suffix_separator + "%SYS%"]
+            branches += [f"EventInfo.Jet{index+1}_{var}_%SYS% \
+                        -> {analysis}_Jet{index+1}_{var}"
+                         + flags.Analysis.systematics_suffix_separator + "%SYS%"]
 
     # B tagged jets
     for var in [*flags.Analysis.small_R_jet.variables_int_bjets,
@@ -44,13 +36,9 @@ def get_selected_objects_branches_variables(flags, analysis):
             if var in flags.Analysis.small_R_jet.variables_int_bjets:
                 int_variable_names += [f"Jet_b{index+1}_{var}"]
             # Translate the name to an analysis specific convention
-            if slim_variables_with_syst and "pt" not in var and "SF" not in var:
-                branches += [f"EventInfo.Jet_b{index+1}_{var}_NOSYS \
-                            -> {analysis}_Jet_b{index+1}_{var}"]
-            else:
-                branches += [f"EventInfo.Jet_b{index+1}_{var}_%SYS% \
-                            -> {analysis}_Jet_b{index+1}_{var}"
-                             + flags.Analysis.systematics_suffix_separator + "%SYS%"]
+            branches += [f"EventInfo.Jet_b{index+1}_{var}_%SYS% \
+                        -> {analysis}_Jet_b{index+1}_{var}"
+                         + flags.Analysis.systematics_suffix_separator + "%SYS%"]
 
     # Photons
     for var in [*flags.Analysis.Photon.variables,
@@ -64,13 +52,9 @@ def get_selected_objects_branches_variables(flags, analysis):
             if var in flags.Analysis.Photon.variables_int:
                 int_variable_names += [f"Photon{index+1}_{var}"]
             # Translate the name to an analysis specific convention
-            if slim_variables_with_syst and "pt" not in var and "SF" not in var:
-                branches += [f"EventInfo.Photon{index+1}_{var}_NOSYS \
-                            -> {analysis}_Photon{index+1}_{var}"]
-            else:
-                branches += [f"EventInfo.Photon{index+1}_{var}_%SYS% \
-                            -> {analysis}_Photon{index+1}_{var}"
-                             + flags.Analysis.systematics_suffix_separator + "%SYS%"]
+            branches += [f"EventInfo.Photon{index+1}_{var}_%SYS% \
+                        -> {analysis}_Photon{index+1}_{var}"
+                         + flags.Analysis.systematics_suffix_separator + "%SYS%"]
 
     # Electrons
     for var in [*flags.Analysis.Electron.variables,
@@ -84,13 +68,9 @@ def get_selected_objects_branches_variables(flags, analysis):
             if var in flags.Analysis.Electron.variables_int:
                 int_variable_names += [f"Electron{index+1}_{var}"]
             # Translate the name to an analysis specific convention
-            if slim_variables_with_syst and "pt" not in var and "SF" not in var:
-                branches += [f"EventInfo.Electron{index+1}_{var}_NOSYS \
-                            -> {analysis}_Electron{index+1}_{var}"]
-            else:
-                branches += [f"EventInfo.Electron{index+1}_{var}_%SYS% \
-                            -> {analysis}_Electron{index+1}_{var}"
-                             + flags.Analysis.systematics_suffix_separator + "%SYS%"]
+            branches += [f"EventInfo.Electron{index+1}_{var}_%SYS% \
+                        -> {analysis}_Electron{index+1}_{var}"
+                         + flags.Analysis.systematics_suffix_separator + "%SYS%"]
 
     # Muons
     for var in [*flags.Analysis.Muon.variables,
@@ -104,13 +84,9 @@ def get_selected_objects_branches_variables(flags, analysis):
             if var in flags.Analysis.Muon.variables_int:
                 int_variable_names += [f"Muon{index+1}_{var}"]
             # Translate the name to an analysis specific convention
-            if slim_variables_with_syst and "pt" not in var and "SF" not in var:
-                branches += [f"EventInfo.Muon{index+1}_{var}_NOSYS \
-                            -> {analysis}_Muon{index+1}_{var}"]
-            else:
-                branches += [f"EventInfo.Muon{index+1}_{var}_%SYS% \
-                            -> {analysis}_Muon{index+1}_{var}"
-                             + flags.Analysis.systematics_suffix_separator + "%SYS%"]
+            branches += [f"EventInfo.Muon{index+1}_{var}_%SYS% \
+                        -> {analysis}_Muon{index+1}_{var}"
+                         + flags.Analysis.systematics_suffix_separator + "%SYS%"]
 
     # Taus
     for var in [*flags.Analysis.Tau.variables,
@@ -124,13 +100,9 @@ def get_selected_objects_branches_variables(flags, analysis):
             if var in flags.Analysis.Tau.variables_int:
                 int_variable_names += [f"Tau{index+1}_{var}"]
             # Translate the name to an analysis specific convention
-            if slim_variables_with_syst and "pt" not in var and "SF" not in var:
-                branches += [f"EventInfo.Tau{index+1}_{var}_NOSYS \
-                            -> {analysis}_Tau{index+1}_{var}"]
-            else:
-                branches += [f"EventInfo.Tau{index+1}_{var}_%SYS% \
-                            -> {analysis}_Tau{index+1}_{var}"
-                             + flags.Analysis.systematics_suffix_separator + "%SYS%"]
+            branches += [f"EventInfo.Tau{index+1}_{var}_%SYS% \
+                        -> {analysis}_Tau{index+1}_{var}"
+                         + flags.Analysis.systematics_suffix_separator + "%SYS%"]
 
     # Selected Lepton
     for var in [*flags.Analysis.Lepton.variables,
@@ -144,12 +116,8 @@ def get_selected_objects_branches_variables(flags, analysis):
             if var in flags.Analysis.Lepton.variables_int:
                 int_variable_names += [f"Lepton{index_str}_{var}"]
             # Translate the name to an analysis specific convention
-            if slim_variables_with_syst and "pt" not in var and "SF" not in var:
-                branches += [f"EventInfo.Lepton{index_str}_{var}_NOSYS \
-                            -> {analysis}_Lepton{index_str}_{var}"]
-            else:
-                branches += [f"EventInfo.Lepton{index_str}_{var}_%SYS% \
-                            -> {analysis}_Lepton{index_str}_{var}"
-                             + flags.Analysis.systematics_suffix_separator + "%SYS%"]
+            branches += [f"EventInfo.Lepton{index_str}_{var}_%SYS% \
+                        -> {analysis}_Lepton{index_str}_{var}"
+                         + flags.Analysis.systematics_suffix_separator + "%SYS%"]
 
     return branches, float_variable_names, int_variable_names
