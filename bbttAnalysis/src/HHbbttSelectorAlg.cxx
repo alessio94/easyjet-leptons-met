d95ee942c6e50f9f41d1710fc301a458b54da72f
Thomas Strebler <thomas.strebler@cern.ch>
2024-07-30 10:25:14 +0200

    Add mvis mass cut for bbtt (#237)

--- bbttAnalysis/src/HHbbttSelectorAlg.cxx
+++ bbttAnalysis/src/HHbbttSelectorAlg.cxx
@@ -318,6 +318,17 @@ namespace HHBBTT
         tau_DR_L1Topo = tau0->p4().DeltaR(tau1->p4())<2.5;
       }
 
+      TLorentzVector tautau_vis;
+      if (n_leptons == 1 && n_taus == 1) {
+        tautau_vis = tau0->p4();
+        if(ele0) tautau_vis += ele0->p4();
+        else if(mu0) tautau_vis += mu0->p4();
+      }
+      else if (n_taus == 2) {
+        tautau_vis = tau0->p4() + tau1->p4();
+      }
+      m_bools.at(HHBBTT::MTAUTAU_VIS_MASS) = tautau_vis.M() > 40. * Athena::Units::GeV;
+
       const xAOD::Jet* jet0 = jets->size()>0 ? jets->at(0) : nullptr;
       const xAOD::Jet* jet1 = jets->size()>1 ? jets->at(1) : nullptr;
 
@@ -407,6 +418,7 @@ namespace HHBBTT
 
       if (m_bools.at(HHBBTT::N_LEPTONS_CUT_LEPHAD) &&
 	  m_bools.at(HHBBTT::ONE_TAU) &&
+	  m_bools.at(HHBBTT::MTAUTAU_VIS_MASS) &&
 	  m_bools.at(HHBBTT::TWO_JETS)){
         // SLT
         if (lep_ptcut_SLT && tau_ptcut_SLT){
@@ -435,6 +447,7 @@ namespace HHBBTT
 
       if (m_bools.at(HHBBTT::N_LEPTONS_CUT_HADHAD) &&
 	  m_bools.at(HHBBTT::TWO_TAU) &&
+	  m_bools.at(HHBBTT::MTAUTAU_VIS_MASS) &&
 	  m_bools.at(HHBBTT::TWO_JETS)){
         // STT
         if (tau_ptcut_STT){
