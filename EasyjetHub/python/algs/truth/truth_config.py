9d6e0010374654a14a06e8744d54863432fbd3ba
Thomas Strebler <thomas.strebler@cern.ch>
2024-08-07 17:38:20 +0200

    Rationalise TruthParentDecorator configs

--- EasyjetHub/python/algs/truth/truth_config.py
+++ EasyjetHub/python/algs/truth/truth_config.py
@@ -1,12 +1,7 @@
 from AthenaConfiguration.ComponentAccumulator import ComponentAccumulator
 from AthenaConfiguration.ComponentFactory import CompFactory
 from EasyjetHub.steering.utils.log_helper import log
-from EasyjetHub.algs.truth.jet_parent_decorator_config import jet_parent_decorator_cfg
-from EasyjetHub.algs.truth.tau_parent_decorator_config import tau_parent_decorator_cfg
-from EasyjetHub.algs.truth.muon_parent_decorator_config import muon_parent_decorator_cfg
-from EasyjetHub.algs.truth.electron_parent_decorator_config import (
-    electron_parent_decorator_cfg
-)
+from EasyjetHub.algs.truth.parent_decorator_config import parent_decorator_cfg
 from EasyjetHub.algs.truth_particle_info_config import truth_particle_info_cfg
 
 
@@ -15,57 +10,53 @@ def truth_info_cfg(
 ):
     cfg = ComponentAccumulator()
 
-    if flags.Analysis.do_jet_parent_decoration and not flags.Input.isPHYSLITE:
+    if not flags.Input.isPHYSLITE:
         # truth record seems to be broken in physlite
-        if flags.Analysis.do_small_R_jets:
-            cfg.merge(jet_parent_decorator_cfg(
+        if flags.Analysis.do_small_R_jets \
+           and flags.Analysis.small_R_jet.do_parent_decoration:
+            cfg.merge(parent_decorator_cfg(
                 flags,
-                jet_collection=flags.Analysis.container_names.input[
+                targetContainer=flags.Analysis.container_names.input[
                     flags.Analysis.small_R_jet.jet_type
                 ],
-                name_prefix="smallR",
-                match_dr=0.3
+                prefix="smallR"
             ))
 
-        if flags.Analysis.do_large_R_UFO_jets:
-            cfg.merge(jet_parent_decorator_cfg(
+        if flags.Analysis.do_large_R_UFO_jets and \
+           flags.Analysis.large_R_jet.do_parent_decoration:
+            cfg.merge(parent_decorator_cfg(
                 flags,
-                jet_collection=flags.Analysis.container_names.input.reco10UFOJet,
-                name_prefix="largeR",
-                match_dr=1.0,
+                targetContainer=flags.Analysis.container_names.input.reco10UFOJet,
+                prefix="largeR",
+                matchDeltaR=1.0,
             ))
 
-        if flags.Analysis.do_large_R_Topo_jets:
-            cfg.merge(jet_parent_decorator_cfg(
+        if flags.Analysis.do_large_R_Topo_jets and \
+           flags.Analysis.large_R_jet.do_parent_decoration:
+            cfg.merge(parent_decorator_cfg(
                 flags,
-                jet_collection=flags.Analysis.container_names.input.reco10TopoJet,
-                name_prefix="largeRTopo",
-                match_dr=1.0,
+                targetContainer=flags.Analysis.container_names.input.reco10TopoJet,
+                prefix="largeRTopo",
+                matchDeltaR=1.0,
             ))
 
-    if flags.Analysis.do_tau_parent_decoration and not flags.Input.isPHYSLITE:
-        cfg.merge(tau_parent_decorator_cfg(
-            flags,
-            tau_collection=flags.Analysis.container_names.input.taus,
-            name_prefix="Tau",
-            match_dr=0.3,
-        ))
+        if flags.Analysis.Tau.do_parent_decoration:
+            cfg.merge(parent_decorator_cfg(
+                flags, targetContainer=flags.Analysis.container_names.input.taus,
+                prefix="Tau"
+            ))
 
-    if flags.Analysis.do_muon_parent_decoration and not flags.Input.isPHYSLITE:
-        cfg.merge(muon_parent_decorator_cfg(
-            flags,
-            muon_collection=flags.Analysis.container_names.input.muons,
-            name_prefix="Muon",
-            match_dr=0.3,
-        ))
+        if flags.Analysis.Muon.do_parent_decoration:
+            cfg.merge(parent_decorator_cfg(
+                flags, targetContainer=flags.Analysis.container_names.input.muons,
+                prefix="Muon"
+            ))
 
-    if flags.Analysis.do_electron_parent_decoration and not flags.Input.isPHYSLITE:
-        cfg.merge(electron_parent_decorator_cfg(
-            flags,
-            electron_collection=flags.Analysis.container_names.input.electrons,
-            name_prefix="Electron",
-            match_dr=0.3,
-        ))
+        if flags.Analysis.Electron.do_parent_decoration:
+            cfg.merge(parent_decorator_cfg(
+                flags, targetContainer=flags.Analysis.container_names.input.electrons,
+                prefix="Electron"
+            ))
 
     log.info("Adding truth particle info seq")
     cfg.merge(
