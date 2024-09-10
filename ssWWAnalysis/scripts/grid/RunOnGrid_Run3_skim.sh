000c9bdbbc8c567b87274277940064e7a5f82774
Yi Yu <yi.yu@cern.ch>
2024-09-10 21:50:19 +0800

    update Run3 samples

--- ssWWAnalysis/scripts/grid/RunOnGrid_Run3_skim.sh
+++ ssWWAnalysis/scripts/grid/RunOnGrid_Run3_skim.sh
@@ -1,16 +1,18 @@
-runConfig="ssWWAnalysis/RunConfig-ssWW-VBS.yaml"
+runConfig="ssWWAnalysis/RunConfig-Signal-ssWW.yaml"
 executable="ssWW-ntupler"
 campaignName="ssWWVBS_v00"
 
-dir_samples="../easyjet/ssWWAnalysis/datasets/PHYSLITE/"
+dir_samples="../easyjet/ssWWAnalysis/datasets/PHYS/"
 mc_list=(
-    "$dir_samples/mc23_Zjet_background_p6026.txt"
-    "$dir_samples/mc23_Wjet_background_p6026.txt"
-    "$dir_samples/mc23_top_background_p6026.txt"
+    "$dir_samples/mc23_Vjets_p6266.txt"
+    "$dir_samples/mc23_Vgamma_p6266.txt"
+    "$dir_samples/mc23_Top_p6266.txt"
+    "$dir_samples/mc23_ZZ_p6266.txt"
+    "$dir_samples/mc23_QCDWZ_p6266.txt"
 )
 
 #data 
-easyjet-gridsubmit --data-list $dir_samples/data_Run3_p6026.txt \
+easyjet-gridsubmit --data-list $dir_samples/data_Run3_p6266.txt \
     --run-config ${runConfig} \
     --exec ${executable} \
     --nGBperJob 5 \
