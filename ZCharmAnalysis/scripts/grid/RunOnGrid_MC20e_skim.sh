f688922d7885bf995af4091dae37ec7cc06734fe
Yi Yu <yi.yu@cern.ch>
2024-09-16 19:16:05 +0200

    update run3 samples for ZCharm

--- ZCharmAnalysis/scripts/grid/RunOnGrid_MC20e_skim.sh
+++ ZCharmAnalysis/scripts/grid/RunOnGrid_MC20e_skim.sh
@@ -1,6 +1,8 @@
+# "ZCharm_v01" resolved + boosted channel
+
 runConfig="ZCharmAnalysis/RunConfig-ZCharm.yaml"
 executable="ZCharm-ntupler"
-campaignName="ZCharm_v00"
+campaignName="ZCharm_v01"
 
 dir_samples="../easyjet/ZCharmAnalysis/datasets/PHYS/MC20e"
 mc_list=(
@@ -8,17 +10,18 @@ mc_list=(
     "$dir_samples/Diboson_Run2_p6266.txt"
     "$dir_samples/ZH_Run2_p6266.txt"
     "$dir_samples/Ttbar_Run2_p6266.txt"
+    "$dir_samples/Zjets_MG_Run2_p6266.txt"
+    "$dir_samples/Stop_Run2_p6266.txt"
 )
-#   "$dir_samples/Zjets_MG_Run2_p6266.txt"
-#   "$dir_samples/Stop_Run2_p6266.txt"
 
-# #data 
-# easyjet-gridsubmit --data-list ../easyjet/ZCharmAnalysis/datasets/PHYS/MC20e/Data_Run2_p6266.txt \
-#     --run-config ${runConfig} \
-#     --exec ${executable} \
-#     --nGBperJob 5 \
-#     --campaign ${campaignName} \
-#     --noTag
+
+#data 
+easyjet-gridsubmit --data-list $dir_samples/Data_Run2_p6266.txt \
+    --run-config ${runConfig} \
+    --exec ${executable} \
+    --nGBperJob 5 \
+    --campaign ${campaignName} \
+    --noTag
 
 # mc
 easyjet-gridsubmit --mc-list <(cat "${mc_list[@]}") \
--- ZCharmAnalysis/scripts/grid/RunOnGrid_Run3_unskimmed.sh
+++ ZCharmAnalysis/scripts/grid/RunOnGrid_Run3_unskimmed.sh
@@ -4,14 +4,14 @@ campaignName="ZCharm_v00"
 
 dir_samples="../easyjet/ZCharmAnalysis/datasets/PHYS/"
 mc_list=(
-    "$dir_samples/Zjets_Run2_p6266.txt"
-    "$dir_samples/Diboson_Run2_p6266.txt"
-    "$dir_samples/ZH_Run2_p6266.txt"
-    "$dir_samples/Top_Run2_p6266.txt"
+    "$dir_samples/mc23_Zjets_MG_p6266.txt"
+    "$dir_samples/mc23_VV_p6266.txt"
+    "$dir_samples/mc23_ttbar_p6266.txt"
+    "$dir_samples/mc23_stop_p6266.txt"
 )
 
 #data 
-easyjet-gridsubmit --data-list $dir_samples/data_Run2_p6266.txt \
+easyjet-gridsubmit --data-list $dir_samples/data_Run3_p6266.txt \
     --run-config ${runConfig} \
     --exec ${executable} \
     --nGBperJob 5 \
