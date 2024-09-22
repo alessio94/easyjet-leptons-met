24413783d0267b3aa5386fac55751504435af9bb
Yi Yu <yi.yu@cern.ch>
2024-09-22 20:53:21 +0800

    fix typo in easyjet-gridsubmit

--- ssWWAnalysis/scripts/grid/RunOnGrid_Run2_skim.sh
+++ ssWWAnalysis/scripts/grid/RunOnGrid_Run2_skim.sh
@@ -2,15 +2,21 @@ runConfig="ssWWAnalysis/RunConfig-Signal-ssWW.yaml"
 executable="ssWW-ntupler"
 campaignName="ssWWVBS_v00"
 
-dir_samples="../easyjet/ssWWAnalysis/datasets/PHYSLITE/"
+dir_samples="../easyjet/ssWWAnalysis/datasets/PHYS/"
 mc_list=(
-    "$dir_samples/mc20_Zjet_background_p6026.txt"
-    "$dir_samples/mc20_Wjet_background_p6026.txt"
-    "$dir_samples/mc20_top_background_p6026.txt"
+    "$dir_samples/mc20_ssWW_MGHW_p6266.txt"
+    "$dir_samples/mc20_ssWW_Sh222_p6266.txt"
+    "$dir_samples/mc20_ssWW_Sh3_p6266.txt"
+    "$dir_samples/mc20_EWWZ_p6266.txt"
+    "$dir_samples/mc20_QCDWZ_p6266.txt"
+    "$dir_samples/mc20_TopV_p6266.txt"
+    "$dir_samples/mc20_Vgamma_p6266.txt"
+    "$dir_samples/mc20_Vjets_p6266.txt"
+    "$dir_samples/mc20_ZZ_p6266.txt"
 )
 
 #data 
-easyjet-gridsubmit --data-list $dir_samples/data_Run2_p6026.txt \
+easyjet-gridsubmit --data-list $dir_samples/data_Run2_p6266.txt \
     --run-config ${runConfig} \
     --exec ${executable} \
     --nGBperJob 5 \
