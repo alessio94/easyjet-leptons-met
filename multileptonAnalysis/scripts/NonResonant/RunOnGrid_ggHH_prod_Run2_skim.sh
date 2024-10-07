8588ae86abfe91dff664e3c63333636c7554baa6
Cen Mo <cen.mo@cern.ch>
2024-10-07 08:40:02 +0200

    [HHML] [First Production] Change tau id wp from loose to medium

--- multileptonAnalysis/scripts/NonResonant/RunOnGrid_ggHH_prod_Run2_skim.sh
+++ multileptonAnalysis/scripts/NonResonant/RunOnGrid_ggHH_prod_Run2_skim.sh
@@ -1,25 +1,40 @@
 runConfig="multileptonAnalysis/RunConfig-multilepton.yaml"
 executable="hhml-ntupler"
-campaignName="HHML_v00"
+campaignName="HHML_v01"
 
-dir_samples="../easyjet/multileptonAnalysis/datasets/PHYSLITE/"
+dir_samples="../easyjet/multileptonAnalysis/datasets/PHYS/prod_v1"
 mc_list=(
-    "$dir_samples/mc20_ggFXHH_samples_p5855.txt"
-    "$dir_samples/mc20_Zjet_background_p6026.txt"
-    "$dir_samples/mc20_Wjet_background_p6026."
-    "$dir_samples/mc20_top_background_p6026.txt"
+    "$dir_samples/mc20_13TeV.HHML.txt"
+    "$dir_samples/mc20_13TeV.Diboson.txt"
+    "$dir_samples/mc20_13TeV.Triboson.txt"
+    "$dir_samples/mc20_13TeV.SingleTop.txt"
+    "$dir_samples/mc20_13TeV.VBF_Higgs.txt"
+    "$dir_samples/mc20_13TeV.VH.txt"
+    "$dir_samples/mc20_13TeV.Vgamma.txt"
+    "$dir_samples/mc20_13TeV.Vjets.txt"
+    "$dir_samples/mc20_13TeV.ggF_Higgs.txt"
+    "$dir_samples/mc20_13TeV.tWZ.txt"
+    "$dir_samples/mc20_13TeV.tZ.txt"
+    "$dir_samples/mc20_13TeV.ttH.txt"
+    "$dir_samples/mc20_13TeV.ttV.txt"
+    "$dir_samples/mc20_13TeV.ttVV.txt"
+    "$dir_samples/mc20_13TeV.ttbar.txt"
+    "$dir_samples/mc20_13TeV.ttt.txt"
+    "$dir_samples/mc20_13TeV.tttt.txt"
 )
 
 #data 
-easyjet-gridsubmit --data-list $dir_samples/data_Run2_p6026.txt \
+easyjet-gridsubmit --data-list $dir_samples/data_Run2.txt \
     --run-config ${runConfig} \
     --exec ${executable} \
     --campaign ${campaignName} \
-    --noTag
+    --noTag \
+    --HDBSProductionRole
 
 #mc
 easyjet-gridsubmit --mc-list <(cat "${mc_list[@]}") \
     --run-config ${runConfig} \
     --exec ${executable} \
     --campaign ${campaignName} \
-    --noTag
+    --noTag \
+    --HDBSProductionRole
