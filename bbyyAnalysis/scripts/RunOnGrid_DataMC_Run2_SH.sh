aa6b86502dd3ea77936953a7c45e0d42c3008fef
Christina Dimitriadi <christina.dimitriadi@cern.ch>
2024-11-06 12:08:38 +0100

    Modified the run configs for 1btag and 2btag regimes, updated grid submission...

--- bbyyAnalysis/scripts/RunOnGrid_DataMC_Run2_SH.sh
+++ bbyyAnalysis/scripts/RunOnGrid_DataMC_Run2_SH.sh
@@ -1,14 +1,15 @@
-runConfig="bbyyAnalysis/RunConfig-Resonant-Default.yaml"
+regime="2btag" #choose between 1btag and 2btag
+runConfig="bbyyAnalysis/RunConfig-Resonant-Default-$regime.yaml"
 executable="bbyy-ntupler"
-campaignName="SHbbyy_vXXX"
+campaignName="SHbbyy_vXXX_$regime"
 dir_PHYS="../easyjet/bbyyAnalysis/datasets/PHYS/nominal"
 dir_PHYSLITE="../easyjet/bbyyAnalysis/datasets/PHYSLITE/nominal"
 mc_campaign="mc20_13TeV"
-ptag=p5855
+ptag="p6266"
 mc_list=(
-    "$dir_PHYS/$mc_campaign.XHS.p6026.txt"
+    "$dir_PHYSLITE/$mc_campaign.SHbbyy_$regime.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.ggFHH_bbyy_SM.$ptag.txt"
-    "$dir_PHYSLITE/$mc_campaign.VBFHH_bbyy_kl1kvv1kv1.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.VBFHH_bbyy_SM.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.ggFH_yy.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.VBFH_yy.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.WpH_yy.$ptag.txt"
@@ -16,21 +17,26 @@ mc_list=(
     "$dir_PHYSLITE/$mc_campaign.qqZH_yy.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.ggZH_yy.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.ttH_yy.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.bbH_yy.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.tHjb.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.tWHyy.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.yyjets.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.yybb.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.ttyy_nonallhad.$ptag.txt"
 )
 #data 
-easyjet-gridsubmit --data-list ../easyjet/bbyyAnalysis/datasets/PHYSLITE/nominal/data_13TeV.Run2.p5855.txt \
+easyjet-gridsubmit --data-list ../easyjet/bbyyAnalysis/datasets/PHYSLITE/nominal/data_13TeV.Run2.p6266.txt \
     --run-config ${runConfig} \
     --exec ${executable} \
+    --nGBperJob 100 \
+    --nCore 3 \
     --campaign ${campaignName}
 
 #mc
 easyjet-gridsubmit --mc-list <(sed -e '$a\' "${mc_list[@]}") \
     --run-config ${runConfig} \
     --exec ${executable} \
-    --nGBperJob 2 \
+    --nGBperJob 10 \
+    --nCore 3 \
     --campaign ${campaignName}
 
--- bbyyAnalysis/scripts/RunOnGrid_DataMC_Run3_SH.sh
+++ bbyyAnalysis/scripts/RunOnGrid_DataMC_Run3_SH.sh
@@ -1,27 +1,35 @@
-runConfig="bbyyAnalysis/RunConfig-Resonant-Default.yaml"
+regime="2btag" #choose between 1btag and 2btag
+runConfig="bbyyAnalysis/RunConfig-Resonant-Default-$regime.yaml"
 executable="bbyy-ntupler"
 campaignName="SHbbyy_vXXX"
 dir_PHYS="../easyjet/bbyyAnalysis/datasets/PHYS/nominal"
 dir_PHYSLITE="../easyjet/bbyyAnalysis/datasets/PHYSLITE/nominal"
 mc_campaign="mc23_13p6TeV"
-ptag=p5855
+ptag="p6266"
 mc_list=(
-    "$dir_PHYS/$mc_campaign.XHS.p6026.txt"
+    "$dir_PHYSLITE/$mc_campaign.SHbbyy_$regime.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.ggFHH_bbyy_SM.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.VBFHH_bbyy_SM.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.ggFH_yy.$ptag.txt"
-    "$dir_PHYSLITE/$mc_campaign.ggZH_yy.$ptag.txt"
-    "$dir_PHYSLITE/$mc_campaign.qqZH_yy.$ptag.txt"
-    "$dir_PHYSLITE/$mc_campaign.ttH_yy.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.VBFH_yy.$ptag.txt"
-    "$dir_PHYSLITE/$mc_campaign.WmH_yy.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.WpH_yy.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.WmH_yy.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.qqZH_yy.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.ggZH_yy.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.ttH_yy.$ptag.txt"
     "$dir_PHYSLITE/$mc_campaign.bbH_yy.$ptag.txt"
-    "$dir_PHYSLITE/mc21_13p6TeV.yyjets.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.tHjb.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.tWHyy.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.yyjets.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.yybb.$ptag.txt"
+    "$dir_PHYSLITE/$mc_campaign.ttyy_nonallhad.$ptag.txt"
 )
 #data 
-easyjet-gridsubmit --data-list ../easyjet/bbyyAnalysis/datasets/PHYSLITE/nominal/data_13p6TeV.Run3.p5858.txt \
+easyjet-gridsubmit --data-list ../easyjet/bbyyAnalysis/datasets/PHYS/nominal/data_13p6TeV.Run3.p6269.txt \
     --run-config ${runConfig} \
     --exec ${executable} \
+    --nGBperJob 100 \
+    --nCore 3 \
     --campaign ${campaignName}
 
 
@@ -29,6 +37,7 @@ easyjet-gridsubmit --data-list ../easyjet/bbyyAnalysis/datasets/PHYSLITE/nominal
 easyjet-gridsubmit --mc-list <(sed -e '$a\' "${mc_list[@]}") \
     --run-config ${runConfig} \
     --exec ${executable} \
-    --nGBperJob 2 \
+    --nGBperJob 10 \
+    --nCore 3 \
     --campaign ${campaignName}
 
