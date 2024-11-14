3ac13788e5f8cf00bca42284e6856e3af528aa4a
Giuseppe Callea <giuseppe.callea@cern.ch>
2024-11-14 15:00:22 +0100

    Adding the bbll PNN score

---
+++ bbllAnalysis/src/ResonantPNNbbllAlg.h
@@ -0,0 +1,93 @@
+/*
+  Copyright (C) 2002-2024 CERN for the benefit of the ATLAS collaboration
+*/
+
+// Always protect against multiple includes!
+#ifndef HHBBLLANALYSIS__ResonantPNNbbllAlg
+#define HHBBLLANALYSIS__ResonantPNNbbllAlg 
+
+#include <xAODEventInfo/EventInfo.h>
+#include <xAODJet/JetContainer.h>
+#include <xAODMuon/MuonContainer.h>
+#include <xAODEgamma/ElectronContainer.h>
+#include <xAODMissingET/MissingETContainer.h>
+#include "lwtnn/LightweightGraph.hh"
+#include "lwtnn/LightweightNeuralNetwork.hh"
+#include <SystematicsHandles/SysReadHandle.h>
+#include <SystematicsHandles/SysListHandle.h>
+#include <AthenaBaseComps/AthHistogramAlgorithm.h>
+#include <SystematicsHandles/SysReadDecorHandle.h>
+#include <SystematicsHandles/SysWriteDecorHandle.h>
+#include <iostream>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/sequenced_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include "NeutrinoWeightingTool.h"
+using namespace lwt;
+namespace HHBBLL
+{
+
+  /// \brief An algorithm for counting containers
+  class ResonantPNNbbllAlg final : public AthHistogramAlgorithm
+  {
+    /// \brief The standard constructor
+  public:
+    ResonantPNNbbllAlg(const std::string &name, ISvcLocator *pSvcLocator);
+
+    /// \brief Initialisation method, for setting up tools and other persistent
+    /// configs
+    StatusCode initialize() override;
+    /// \brief Execute method, for actions to be taken in the event loop
+    StatusCode execute() override;
+    /// We use default finalize() -- this is for cleanup, and we don't do any
+    void LoadPDNN_prepare(size_t total_inputs, const xAOD::EventInfo& event, const xAOD::ElectronContainer& electrons, const xAOD::MuonContainer& muons, const ConstDataVector<xAOD::JetContainer>& bjets, const xAOD::MissingET& met,const CP::SystematicSet& sys);
+
+  private:
+    // ToolHandle<whatever> handle {this, "pythonName", "defaultValue",
+    // "someInfo"};
+
+    /// \brief Setup syst-aware input container handles
+    CP::SysListHandle m_systematicsList {this};
+    CP::SysReadDecorHandle<float>
+      m_met_sig {this, "METSignificance", "significance", "Met Significance"};
+
+    CP::SysReadHandle<xAOD::JetContainer>
+      m_jetHandle{ this, "jets", "bbllAnalysisJets_%SYS%", "Jet container to read" };
+
+    CP::SysReadDecorHandle<char> 
+      m_isBtag {this, "bTagWPDecorName", "", "Name of input dectorator for b-tagging"};
+    
+    CP::SysReadHandle<xAOD::ElectronContainer>
+      m_electronHandle{ this, "electrons", "bbllAnalysisElectrons_%SYS%", "Electron container to read" };
+
+    CP::SysReadHandle<xAOD::MuonContainer>
+      m_muonHandle{ this, "muons", "bbllAnalysisMuons_%SYS%", "Muon container to read" };
+
+    CP::SysReadHandle<xAOD::MissingETContainer>
+    m_metHandle{ this, "met", "AnalysisMET_%SYS%", "MET container to read" };
+
+    CP::SysReadHandle<xAOD::EventInfo>
+    m_eventHandle{ this, "event", "EventInfo", "EventInfo container to read" };
+
+    Gaudi::Property <std::vector<double>> m_mX_values {this, "mX_values", {}, "mX values"};
+
+    Gaudi::Property<std::vector<std::string>> m_floatVariables
+      { this, "floatPNNVariables", {}, "List of float variables to write out" };
+
+    std::unordered_map<std::string, CP::SysWriteDecorHandle<float>>
+      m_Fbranches;
+
+    CP::SysReadDecorHandle<float>
+      m_NW {this, "NW_weight", "", "Neutrino Weighting weight"};
+
+    ToolHandleArray<NeutrinoWeightingTool> m_reco_pairings { this, "NeutrinoWeightingTools", {}, "" };
+
+    const std::string m_PNN_ScoreLabel = "PNN_Score";
+    std::unique_ptr<lwt::LightweightGraph> m_model_PNN_setA;
+    std::unique_ptr<lwt::LightweightGraph> m_model_PNN_setB;
+    std::unique_ptr<lwt::LightweightGraph> m_model_PNN_setC;
+    std::map<std::string, double> pnn_inputs;
+  };
+}
+#endif
