172af936a208b706f3137ab5fc01e34755620869
Georges Aad <georges.aad@cern.ch>
2024-12-07 14:43:45 +0100

    Add ntuple validation to ci

--- EasyjetTests/util/compareOutputNtuples.cxx
+++ EasyjetTests/util/compareOutputNtuples.cxx
@@ -11,6 +11,7 @@
 #include <TLegend.h>
 #include <TSystem.h>
 #include <TEnv.h>
+#include <TROOT.h>
 
 #include <vector>
 #include <set>
@@ -19,32 +20,32 @@
 
 using namespace std;
 
-void drawDiff(TH1* newHisto, TH1* oldHisto, TH1* diff, const TString &branchname, const TString &outdir){
+void drawDiff(TH1* newHisto, TH1* refHisto, TH1* diff, const TString &branchname, const TString &outdir){
 
   TCanvas *cc = new TCanvas(branchname, branchname, 1200, 600);
   cc->Divide(3,1);
-  // diff->SetXTitle(branchname);
   cc->cd(1);
   newHisto->SetXTitle(branchname);
   newHisto->SetYTitle("entries (new)");
   newHisto->Draw();
   newHisto->SetLineWidth(2);
   cc->cd(2);
-  oldHisto->SetXTitle(branchname);
-  oldHisto->SetYTitle("entries (old)");
-  oldHisto->SetLineWidth(2);
-  oldHisto->Draw();
+  refHisto->SetXTitle(branchname);
+  refHisto->SetYTitle("entries (ref)");
+  refHisto->SetLineWidth(2);
+  refHisto->Draw();
   cc->cd(3);
   diff->SetXTitle(branchname);
-  diff->SetYTitle("entries (new-old)");
+  diff->SetYTitle("entries (new-ref)");
   diff->SetLineWidth(2);
   diff->Draw();
   cc->SaveAs(outdir+"/"+branchname+".png");
   cc->SaveAs(outdir+"/"+branchname+".pdf");
+  delete cc;
 
 }
 
-void drawDiffSame(TH1* newHisto, TH1* oldHisto, TH1* diff, const TString& branchname, const TString& outdir){
+void drawDiffSame(TH1* newHisto, TH1* refHisto, TH1* diff, const TString& branchname, const TString& outdir){
 
   TCanvas *cc = new TCanvas(branchname, branchname, 1000, 600);
   cc->Divide(2,1);
@@ -54,29 +55,31 @@ void drawDiffSame(TH1* newHisto, TH1* oldHisto, TH1* diff, const TString& branch
   newHisto->SetLineWidth(2);
   newHisto->SetLineColor(1);
   newHisto->Draw();
-  oldHisto->SetLineColor(2);
-  oldHisto->SetLineWidth(2);
-  oldHisto->SetLineStyle(2);
-  oldHisto->Draw("sames");
+  refHisto->SetLineColor(2);
+  refHisto->SetLineWidth(2);
+  refHisto->SetLineStyle(2);
+  refHisto->Draw("sames");
 
-  float ymax = (newHisto->GetMaximum()>oldHisto->GetMaximum()) ? newHisto->GetMaximum() : oldHisto->GetMaximum();
+  float ymax = (newHisto->GetMaximum()>refHisto->GetMaximum()) ? newHisto->GetMaximum() : refHisto->GetMaximum();
   newHisto->SetMaximum(ymax*1.1);
   
   TLegend *leg = new TLegend(0.6,0.75,0.75,0.85);
   leg->SetBorderSize(0);
   leg->SetFillColor(0);
   leg->AddEntry(newHisto,"new","l");
-  leg->AddEntry(oldHisto,"old","l");
+  leg->AddEntry(refHisto,"ref","l");
   leg->Draw();
 
   cc->cd(2);
   diff->SetXTitle(branchname);
-  diff->SetYTitle("entries (new-old)");
+  diff->SetYTitle("entries (new-ref)");
   diff->SetLineWidth(2);
   diff->Draw();
   
   cc->SaveAs(outdir+"/"+branchname+".png");
   cc->SaveAs(outdir+"/"+branchname+".pdf");
+  delete leg;
+  delete cc;
 
 }
 
@@ -84,18 +87,20 @@ void dumpSet(const set<TString> &tset){
   for(auto a : tset) cout << a << endl;
 }
 
-void compareTrees(TTree* oldTree, TTree* newTree, bool useSameBinning, const TString& outdir){
+void compareTrees(TTree* refTree, TTree* newTree, bool useSameBinning, const TString& outdir){
 
-  cout << "############ comparing " << oldTree->GetName() <<" tree #################" << endl;
-
-  int oldEntries = oldTree->GetEntries();
+  cout << "############ comparing " << refTree->GetName() <<" tree #################" << endl;
+  int maxBranches=1000;
+  int maxPlots=100;
+ 
+  int refEntries = refTree->GetEntries();
   int newEntries = newTree->GetEntries();
 
-  if(oldEntries == newEntries){
-    cout << " old and new trees have the same number of entries = " << newEntries << endl;
+  if(refEntries == newEntries){
+    cout << " ref and new trees have the same number of entries = " << newEntries << endl;
   }
   else{
-    cout << " different number of entries new=" <<newEntries << " old=" << oldEntries << endl;
+    cout << " different number of entries new=" <<newEntries << " ref=" << refEntries << endl;
     cout << "---> all branches will be different " << endl;
   }
 
@@ -103,16 +108,17 @@ void compareTrees(TTree* oldTree, TTree* newTree, bool useSameBinning, const TSt
   set<TString> removedBranchList;
   set<TString> diffBranchList;
   set<TString> okBranchList;
-  
 
   TObjArray *newBranchList = newTree->GetListOfBranches();
+  TObjArray *refBranchList = refTree->GetListOfBranches();
 
+  int nplots=0;
+  int ncbranches=0;
   for(int i=0; i<newBranchList->GetSize(); ++i){
-
-    TBranch *newBranch = dynamic_cast<TBranch*>(newBranchList->At(i));
-    TString branchName = newBranch->GetName();
-
-    if(oldTree->FindBranch(branchName)){
+    
+    TString branchName = newBranchList->At(i)->GetName();
+    
+    if(refTree->FindBranch(branchName)){
 
       /// if at some points we want to add cuts
       TString cut ="";
@@ -122,24 +128,27 @@ void compareTrees(TTree* oldTree, TTree* newTree, bool useSameBinning, const TSt
       //if(!branchName.EndsWith(""))continue;
       //if(!branchName.EqualTo(""))continue;
 
+      if(ncbranches >= maxBranches) continue;
+      ++ncbranches;
+      
       TString newHistoName= branchName+"_new";
-      TString oldHistoNameSB= branchName+"_oldSB";
-      TString oldHistoName= branchName+"_old";
+      TString refHistoNameSB= branchName+"_refSB";
+      TString refHistoName= branchName+"_ref";
 
       newTree->Draw(branchName+">>"+newHistoName,cut,"goff");
       void *newHistoTmp=0;
       gDirectory->GetObject(newHistoName,newHistoTmp);
-      TH1 *newHisto  = static_cast<TH1*>(newHistoTmp);
+      TH1* newHisto  = static_cast<TH1*>(newHistoTmp);
+    
+      TH1* refHistoSB = (TH1*)newHisto->Clone(refHistoNameSB);
+      refTree->Draw(branchName+">>"+refHistoNameSB,cut,"goff");
 
-      TH1* oldHistoSB = (TH1*)newHisto->Clone(oldHistoNameSB);
-      oldTree->Draw(branchName+">>"+oldHistoNameSB,cut,"goff");
+      refTree->Draw(branchName+">>"+refHistoName,cut,"goff");
+      void *refHistoTmp=0;
+      gDirectory->GetObject(refHistoName,refHistoTmp);
+      TH1 *refHisto  = static_cast<TH1*>(refHistoTmp);
 
-      oldTree->Draw(branchName+">>"+oldHistoName,cut,"goff");
-      void *oldHistoTmp=0;
-      gDirectory->GetObject(oldHistoName,oldHistoTmp);
-      TH1 *oldHisto  = static_cast<TH1*>(oldHistoTmp);
-
-      TH1* diff = (TH1*)oldHistoSB->Clone(branchName+"_difference");
+      TH1* diff = (TH1*)refHistoSB->Clone(branchName+"_difference");
       diff->Scale(-1);
       diff->Add(newHisto);
 	
@@ -151,33 +160,47 @@ void compareTrees(TTree* oldTree, TTree* newTree, bool useSameBinning, const TSt
 	}
       }
 
-      if(OK ){
+      if(OK){
 	okBranchList.insert(branchName);
-	//drawDiffSame(newHisto, oldHistoSB, diff, branchName);
-	continue;
-      }
-
-      diffBranchList.insert(branchName);
-
-      if(useSameBinning){
-	drawDiffSame(newHisto, oldHistoSB, diff, branchName, outdir);
       }
       else{
-	drawDiff(newHisto, oldHisto, diff, branchName, outdir);
+	diffBranchList.insert(branchName);
+	if(nplots<maxPlots){
+	  ++nplots;
+	  if(useSameBinning){
+	    drawDiffSame(newHisto, refHistoSB, diff, branchName, outdir);
+	  }
+	  else{
+	    drawDiff(newHisto, refHisto, diff, branchName, outdir);
+	  }
+	}
       }
+
+      gDirectory->Remove(newHisto);
+      gDirectory->Remove(refHistoSB);
+      gDirectory->Remove(refHisto);
+      gDirectory->Remove(diff);
+      
+      newHisto->SetDirectory(0);
+      refHistoSB->SetDirectory(0);
+      refHisto->SetDirectory(0);
+      diff->SetDirectory(0);
+      
+      delete newHisto;
+      delete refHistoSB;
+      delete refHisto;
+      delete diff;
 
     }
     else{
       addedBranchList.insert(branchName);
     }
-
   }
+  cout << endl;
 
-
-  TObjArray *oldBranchList = oldTree->GetListOfBranches();
-  for(int i=0; i<oldBranchList->GetSize(); ++i){
-    TBranch * oldBranch = dynamic_cast<TBranch*>(oldBranchList->At(i));
-    TString branchName = oldBranch->GetName();
+  for(int i=0; i<refBranchList->GetSize(); ++i){
+    TBranch * refBranch = dynamic_cast<TBranch*>(refBranchList->At(i));
+    TString branchName = refBranch->GetName();
     if(!newTree->FindBranch(branchName)){
       removedBranchList.insert(branchName);
     }
@@ -198,17 +221,25 @@ void compareTrees(TTree* oldTree, TTree* newTree, bool useSameBinning, const TSt
   dumpSet(removedBranchList);
   cout <<"----> " << removedBranchList.size() << " branches are removed" << endl;
 
-
-  cout << "List of diff branches:" << endl;
+  cout << "List of modified branches:" << endl;
   cout << "----------------------" << endl;
   dumpSet(diffBranchList);
-  cout <<"----> " << diffBranchList.size() << " branches are diff" << endl;
+  cout <<"----> " << diffBranchList.size() << " branches are different" << endl;
 
-  cout << "############ end comparing " << oldTree->GetName() <<" tree #################" << endl;
+  cout << "Max branches to compare = " << maxBranches << endl;
+  cout << "Max allowed plots = " << maxPlots << endl;
+  
+  std::cout << "Reference has " << refBranchList->GetSize() << " branches "
+	    << " and new has " << newBranchList->GetSize()<< " branches "
+	    << std::endl;
+  
+
+  
+  cout << "############ end comparing " << refTree->GetName() <<" tree #################" << endl;
 
 }
 // stop clang check here. Not using const & since we want this function to be callable from command line using root
-void compareOutputNtuples(TString oldFileName, // NOLINT
+void compareOutputNtuples(TString refFileName, // NOLINT
 			  TString newFileName, // NOLINT
 			  TString treeName="AnalysisMiniTree", // NOLINT
 			  TString outdir="compare_output", // NOLINT
@@ -222,14 +253,13 @@ void compareOutputNtuples(TString oldFileName, // NOLINT
 
   gStyle->SetOptStat(111111);
 
-  TChain *ch1 = new TChain(treeName);
-  TChain *ch2 = new TChain(treeName);
+  TFile* nr = TFile::Open(refFileName);
+  TTree* ch1 = (TTree*)nr->Get(treeName);
+  
+  TFile* nf = TFile::Open(newFileName);
+  TTree* ch2 = (TTree*)nf->Get(treeName);
 
-
-  ch1->Add(newFileName);
-  ch2->Add(oldFileName);
-
-  compareTrees(ch2, ch1, drawsame, outdir);
+  compareTrees(ch1, ch2, drawsame, outdir);
 
 }
 
@@ -237,7 +267,7 @@ void compareOutputNtuples(TString oldFileName, // NOLINT
 #ifndef __CLING__
 
 void usage(const string &ss){
-  cout << "Usage: " << ss << " oldfilename newfilename [treeName] [outdir] " << endl;
+  cout << "Usage: " << ss << " reffilename newfilename [treeName] [outdir] " << endl;
   cout << "default treeName=\"AnalysisMiniTree\" outdir=\"compare_output\" " << endl;
 }
 
@@ -248,7 +278,7 @@ int main(int argc, char **argv){
     return 1;
   }
 
-  TString oldFileName = argv[1];
+  TString refFileName = argv[1];
   TString newFileName = argv[2];
 
   TString treeName="AnalysisMiniTree";
@@ -258,7 +288,7 @@ int main(int argc, char **argv){
   if(argc>4)outdir = argv[4];
 
 
-  compareOutputNtuples(oldFileName, newFileName, treeName, outdir);
+  compareOutputNtuples(refFileName, newFileName, treeName, outdir);
 
   return 0;
 
