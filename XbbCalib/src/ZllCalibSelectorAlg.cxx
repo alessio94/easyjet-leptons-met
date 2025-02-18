8f4b47f557d90e6f2578eac4be5c0c06709a152d
Adam Campbell Anderson <adam.campbell.anderson@cern.ch>
2025-02-18 17:08:09 +0000

    Adding Zll to Xbb Signal Calibration

---
+++ XbbCalib/src/ZllCalibSelectorAlg.cxx
@@ -0,0 +1,101 @@
+/*
+  Copyright (C) 2002-2023 CERN for the benefit of the ATLAS collaboration
+*/
+
+#include "ZllCalibSelectorAlg.h"
+#include <SystematicsHandles/SysFilterReporter.h>
+#include <SystematicsHandles/SysFilterReporterCombiner.h>
+#include <AthenaKernel/Units.h>
+
+namespace XBBCALIB
+{
+
+  ZllCalibSelectorAlg::ZllCalibSelectorAlg(const std::string &name,
+                                ISvcLocator *pSvcLocator)
+      : AthHistogramAlgorithm(name, pSvcLocator)
+  {
+
+  }
+
+  StatusCode ZllCalibSelectorAlg::initialize()
+  {
+    // Initialise global event filter
+    ATH_CHECK (m_filterParams.initialize(m_systematicsList));
+
+    ATH_CHECK (m_eventHandle.initialize(m_systematicsList));
+
+    ATH_CHECK (m_jetHandle.initialize(m_systematicsList));
+    ATH_CHECK (m_lrjetHandle.initialize(m_systematicsList));
+    ATH_CHECK (m_electronHandle.initialize(m_systematicsList));
+    ATH_CHECK (m_muonHandle.initialize(m_systematicsList));
+
+    // Intialise syst list (must come after all syst-aware inputs and outputs)
+    ATH_CHECK (m_systematicsList.initialize()); 
+
+    return StatusCode::SUCCESS;
+  }
+
+
+  StatusCode ZllCalibSelectorAlg::execute()
+  {
+
+    // Global filter originally false
+    CP::SysFilterReporterCombiner filterCombiner (m_filterParams, false);
+
+    // Loop over all systs
+    for (const auto& sys : m_systematicsList.systematicsVector())
+    {
+      CP::SysFilterReporter filter (filterCombiner, sys);
+
+      // Initialize Variables
+      bool pass_baseline = false;
+
+      // Retrive inputs
+      const xAOD::EventInfo *event = nullptr;
+      ANA_CHECK (m_eventHandle.retrieve (event, sys));
+
+      const xAOD::JetContainer *lrjets = nullptr;
+      ANA_CHECK (m_lrjetHandle.retrieve (lrjets, sys));
+
+      const xAOD::MuonContainer *muons = nullptr;
+      ANA_CHECK (m_muonHandle.retrieve (muons, sys));
+      
+      const xAOD::ElectronContainer *electrons = nullptr;
+      ANA_CHECK (m_electronHandle.retrieve (electrons, sys));
+
+      //************
+      // Apply Selection
+      //************
+
+      // flags
+      //************
+      // Large-R Jet
+      //************
+      pass_baseline = (lrjets->size() != 0);
+
+      //************
+      // Leptons
+      //************
+      pass_baseline &= (electrons->size() >= 2 || muons->size() >= 2);
+
+      //****************
+      // event level info
+      //****************
+
+      if (!m_bypass && !pass_baseline) continue;
+
+      // Global event filter true if any syst passes and controls
+      // if event is passed to output writing or not
+      filter.setPassed(true);
+    }
+
+    return StatusCode::SUCCESS;
+  }
+
+  StatusCode ZllCalibSelectorAlg::finalize()
+  {
+    ANA_CHECK (m_filterParams.finalize());
+    return StatusCode::SUCCESS;
+  }
+
+}
--- XbbCalib/src/components/XbbCalib_entries.cxx
+++ XbbCalib/src/components/XbbCalib_entries.cxx
@@ -1,11 +1,16 @@
 #include "../BaselineVarsXbbCalibAlg.h"
 #include "../BaselineVarsZbbjCalibAlg.h"
+#include "../BaselineVarsZllCalibAlg.h"
 #include "../XbbCalibSelectorAlg.h"
 #include "../ZbbjCalibSelectorAlg.h"
+#include "../ZllCalibSelectorAlg.h"
+
 
 using namespace XBBCALIB;
 
 DECLARE_COMPONENT(BaselineVarsXbbCalibAlg)
 DECLARE_COMPONENT(BaselineVarsZbbjCalibAlg)
+DECLARE_COMPONENT(BaselineVarsZllCalibAlg)
 DECLARE_COMPONENT(XbbCalibSelectorAlg)
 DECLARE_COMPONENT(ZbbjCalibSelectorAlg)
+DECLARE_COMPONENT(ZllCalibSelectorAlg)
