808e3b0609257fe4268c36747cde487ab179dcf2
Storm Lin <storm.lin@stonybrook.edu>
2025-05-05 13:19:06 +0000

    Add particle origin and type to truth tau variables

--- EasyjetHub/python/output/ttree/truth_taus.py
+++ EasyjetHub/python/output/ttree/truth_taus.py
@@ -20,6 +20,7 @@ def get_truthtau_branches(flags, input_container, output_prefix):
     truthtau_branches.variables = \
         ["px", "py", "pz", "e", "m", "numCharged", "IsHadronicTau"] \
         + [var + "_vis" for var in ["pt", "eta", "phi", "m"]] \
-        + ["decay_vertex_" + var for var in ["x", "y", "z"]]
+        + ["decay_vertex_" + var for var in ["x", "y", "z"]] \
+        + ["classifierParticleOrigin", "classifierParticleType"]
 
     return truthtau_branches.get_output_list()
