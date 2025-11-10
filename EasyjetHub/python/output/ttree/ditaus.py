e578c4d4f41c66aa7d6722bfbe6c248871f4df1f
Giacomo Magni <giacomo.magni@cern.ch>
2025-11-10 14:55:20 +0100

    Update DiTau configuaration

--- EasyjetHub/python/output/ttree/ditaus.py
+++ EasyjetHub/python/output/ttree/ditaus.py
@@ -27,7 +27,7 @@ def get_ditau_branches(flags, tree_flags, input_container, output_prefix):
         for id_wp in id_wps
     ]
 
-    if flags.Analysis.DiTau.do_score_decoration:
+    if flags.Analysis.DiTau.score_branches:
         ditau_branches.variables += ["omni_score"]
 
     return ditau_branches.get_output_list()
