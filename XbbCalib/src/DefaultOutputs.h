9ed7f83e032419d26360db9610f75485ba0f550d
Dan Guest <daniel.hay.guest@cern.ch>
2025-11-13 02:56:24 +0000

    Change default values for floats and ints

--- XbbCalib/src/DefaultOutputs.h
+++ XbbCalib/src/DefaultOutputs.h
@@ -4,15 +4,25 @@
 #include <concepts>
 
 namespace XBBCALIB::defaults {
-  // To do: make this somethig more standard like NAN
-  const float def_float = -99;
-  // To do: make this something more standard like -1
-  const int def_int = -99;
+  // A valid float can take on pretty much any value in (-inf, inf),
+  // NAN is usually the obvious "wrong" value.
+  const float def_float = NAN;
+
+  // Int is often used for counting, in which case -1 isn't a bad
+  // default.
+  const int def_int = -1;
+
+  // for unsigned values this is a lot less clear, but it shouldn't be -1
+  const unsigned int def_unsigned_int = 0;
 
   // Overloaded templates to get defaults for a generic type T
-  template <std::integral T>
+  template <std::signed_integral T>
   auto get_default() {
     return def_int;
+  }
+  template <std::unsigned_integral T>
+  auto get_default() {
+    return def_unsigned_int;
   }
   template <std::floating_point T>
   auto get_default() {
